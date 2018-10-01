2fff5f3bfe1e2711d7dcadbf9c6c4909f2760f6d
Alexey <alexey.faustov@gmail.com>
2018-10-02 01:12:52 +0300

    Avito data scraper and a simple data base creator without relations between tables

---
+++ avito_db.py
@@ -0,0 +1,18 @@
+#from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from avito_db_entities import Base, Category, Advert
+
+# start the DB engine
+engine = create_engine('sqlite:///avito_adv.db', echo=True)
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+Base.metadata.create_all(engine)
+
+if __name__ == '__main__':
+    Advert.__table__.drop(engine)
+    session.add(Advert(advert_id='1', title='hello', href='_', description='akjfhaskhf'))
+    session.commit()
