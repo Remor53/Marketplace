5f08c96ce69009d0ea7a5bb38d061c738aedb3e6
Remor <tiunov.ivan99@mail.ru>
2018-10-06 17:57:36 +0300

    Сделал вьюху с выводом категорий и товаров

--- flask_hw_page.py
+++ flask_hw_page.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+from avito_db_entities import Category, Advert
+import avito_db as db
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/list')
+def c_list():
+    category_number = 0
+    category_list = list()
+    advert_list = list()
+    for category in db.session.query(Category).all():
+        category_list.append(category.name)
+        category_number += 1
+        if category_number > 10:
+            break
+    for advert in db.session.query(Advert).all():
+        advert_list.append({'title': advert.title, 'description': advert.description})
+    return render_template('list.html', category=category_list, advert=advert_list)
 
 
 if __name__ == '__main__':
