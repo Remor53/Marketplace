7c9815978d4c1c4ca94e9822161cf181a957238d
Remor <tiunov.ivan99@mail.ru>
2018-10-15 09:45:41 +0300

    Вьюха для карточки товара и ссылки на эти карточки в списки товаров

--- flask_hw_page.py
+++ flask_hw_page.py
@@ -10,14 +10,27 @@ def c_list():
     category_number = 0
     category_list = list()
     advert_list = list()
+    num = 0
     for category in db.session.query(Category).all():
         category_list.append(category.name)
         category_number += 1
         if category_number > 10:
             break
     for advert in db.session.query(Advert).all():
-        advert_list.append({'title': advert.title, 'description': advert.description})
+        num += 1
+        advert_list.append({'title': advert.title, 'description': advert.description, 'id': num})
     return render_template('list.html', category=category_list, advert=advert_list)
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    advert_item = dict()
+    num = 0
+    for advert in db.session.query(Advert).all():
+            num += 1
+            if num == post_id:
+                advert_item = {'title': advert.title, 'description': advert.description, 'id': num}
+    return render_template('card.html', advert=advert_item)
 
 
 if __name__ == '__main__':
